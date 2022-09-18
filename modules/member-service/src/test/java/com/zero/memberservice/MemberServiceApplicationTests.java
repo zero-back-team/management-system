d0f96289edc0249bece79cd4ee11da79e1056dce
donghun93 <alswn4516@gmail.com>
2022-09-18 17:32:37 +0900

    feat: 기본 디렉토리 구조 생성

---
+++ modules/member-service/src/test/java/com/zero/memberservice/MemberServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero.memberservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MemberServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
