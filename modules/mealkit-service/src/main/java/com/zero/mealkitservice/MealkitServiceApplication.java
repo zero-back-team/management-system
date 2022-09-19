9c6394df305713bfdd835da23a2fbadde666ca8c
donghun93 <alswn4516@gmail.com>
2022-09-19 23:17:25 +0900

    feat: 밀키트팀 모듈 생성 및 초기화

---
+++ modules/mealkit-service/src/main/java/com/zero/mealkitservice/MealkitServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zero.mealkitservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealkitServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MealkitServiceApplication.class, args);
+    }
+
+}
---
+++ modules/mealkit-service/src/test/java/com/zero/mealkitservice/MealkitServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero.mealkitservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MealkitServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
