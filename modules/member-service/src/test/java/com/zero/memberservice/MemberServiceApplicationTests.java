bf0ef9b85b8826bd5235e4d6df4534e01f8742ae
donghun93 <alswn4516@gmail.com>
2022-09-21 22:36:54 +0900

    feat: "사용자 테스트 추가"
    사용자 가입, 조회, 수정, 삭제 테스트

--- modules/member-service/src/test/java/com/zero/memberservice/MemberServiceApplicationTests.java
+++ modules/member-service/src/test/java/com/zero/memberservice/MemberServiceApplicationTests.java
@@ -1,13 +1,52 @@
 package com.zero.memberservice;
 
+import com.zero.memberservice.repository.MemberRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class MemberServiceApplicationTests {
 
+    private MemberRepository memberRepository;
+
     @Test
-    void contextLoads() {
+    @DisplayName("사용자 가입 테스트")
+    void memberInsertTest() {
+        // given
+
+        // when
+
+        // then
     }
 
+    @Test
+    @DisplayName("사용자 조회 테스트")
+    void memberSelectTest() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    @DisplayName("사용자 수정 테스트")
+    void memberUpdateTest() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    @DisplayName("사용자 삭제 테스트")
+    void memberDeleteTest() {
+        // given
+
+        // when
+
+        // then
+    }
 }
