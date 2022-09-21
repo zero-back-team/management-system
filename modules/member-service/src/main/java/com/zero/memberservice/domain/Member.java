a9066e9bd2121134f59712250892202372daa6da
donghun93 <alswn4516@gmail.com>
2022-09-21 22:34:28 +0900

    feat: "사용자 도메인 빌더 추가"
    사용자 도메인 Builder 및 Getter 추가

--- modules/member-service/src/main/java/com/zero/memberservice/domain/Member.java
+++ modules/member-service/src/main/java/com/zero/memberservice/domain/Member.java
@@ -2,12 +2,15 @@ package com.zero.memberservice.domain;
 
 import com.zero.memberservice.type.ForeignType;
 import com.zero.memberservice.type.Grade;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
 
     @Id @GeneratedValue
@@ -19,4 +22,15 @@ public class Member {
     private String passWord;
     private Grade grade; // Bronze, Silver, Gold
     private Long totalAmount;
+
+    @Builder
+    private Member(String email, String birth, ForeignType foreignType, String id, String passWord, Grade grade, Long totalAmount) {
+        this.email = email;
+        this.birth = birth;
+        this.foreignType = foreignType;
+        this.id = id;
+        this.passWord = passWord;
+        this.grade = grade;
+        this.totalAmount = totalAmount;
+    }
 }
