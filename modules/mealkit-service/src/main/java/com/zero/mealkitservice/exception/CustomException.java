1c469a8a8e53880d348d5065fd1f6ca31073f51d
Jinho Kim <66901361+naekang@users.noreply.github.com>
2022-09-23 22:22:47 +0900

    feat: Add custom exception (#11)
    - 커스텀 예외 추가

---
+++ modules/mealkit-service/src/main/java/com/zero/mealkitservice/exception/CustomException.java
@@ -0,0 +1,23 @@
+package com.zero.mealkitservice.exception;
+
+import com.zero.mealkitservice.type.ErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CustomException extends RuntimeException {
+
+    private ErrorCode errorCode;
+    private String errorMessage;
+
+    public CustomException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+        this.errorMessage = errorCode.getDescription();
+    }
+
+}
