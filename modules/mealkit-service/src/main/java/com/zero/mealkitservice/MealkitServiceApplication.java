a91c8d2d08583589deb4085ecf4ba8ec4cc87e14
naekang <rlawlsgh6306@gmail.com>
2022-09-26 22:08:08 +0900

    feat: AWS S3 설정 추가

--- modules/mealkit-service/src/main/java/com/zero/mealkitservice/MealkitServiceApplication.java
+++ modules/mealkit-service/src/main/java/com/zero/mealkitservice/MealkitServiceApplication.java
@@ -8,6 +8,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class MealkitServiceApplication {
 
+    static {
+        System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(MealkitServiceApplication.class, args);
     }
