b5741bd97b0d3b2945294ea957fa7864ffed8578
Jinho Kim <66901361+naekang@users.noreply.github.com>
2022-10-05 22:03:52 +0900

    feat: Add search product & register test (#17)
    - 상품 등록 테스트 Mock 사용해서 변경
    - 상품 조회 기능 및 테스트 작성

--- modules/mealkit-service/src/main/java/com/zero/mealkitservice/dto/ProductDto.java
+++ modules/mealkit-service/src/main/java/com/zero/mealkitservice/dto/ProductDto.java
@@ -12,12 +12,61 @@ import lombok.NoArgsConstructor;
 @Builder
 public class ProductDto {
     private Long id;
+
     private String name;
+
+    private Long price;
+
+    private Long stock;
+
+    private String brand;
+
+    private int calorie;
+
+    private String serving;
+
+    private String category;
+
+    private String briefDescription;
+
+    private String description;
+
+    private String origin;
+
+    private String recipe;
+
+    private String expirationDate;
+
+    private String allergy;
+
+    private String deliveryInfo;
+
+    private String deliveryPeriod;
+
+    private int discountRate;
+
+    private String imageUrl;
 
     public static ProductDto fromEntity(Product product){
         return ProductDto.builder()
                 .id(product.getId())
                 .name(product.getName())
+                .price(product.getPrice())
+                .stock(product.getStock())
+                .brand(product.getBrand())
+                .calorie(product.getCalorie())
+                .serving(product.getServing())
+                .category(product.getCategory())
+                .briefDescription(product.getBriefDescription())
+                .description(product.getDescription())
+                .origin(product.getOrigin())
+                .recipe(product.getRecipe())
+                .expirationDate(product.getExpirationDate())
+                .allergy(product.getAllergy())
+                .deliveryInfo(product.getDeliveryInfo())
+                .deliveryPeriod(product.getDeliveryPeriod())
+                .discountRate(product.getDiscountRate())
+                .imageUrl(product.getImage())
                 .build();
     }
 }
